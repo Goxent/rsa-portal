12100c943575e7b2af98a646d76cc0b7eff2f7c4
Anil Sapkota <Anil99senchury@gmail.com>
2026-04-17 13:20:53 +0545

    Updates

--- assets/firmData-DluVYiRu.js
+++
@@ -1,6 +0,0 @@
-import{c as a}from"./index-XVBkLyaR.js";/**
- * @license lucide-react v0.563.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["path",{d:"m16 11 2 2 4-4",key:"9rsbq5"}],["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}]],c=a("user-check",t),n=["R. Sapkota & Associates","TN Acharya & Co.","Pankaj Thapa Associates","NP Sharma & Co.","Others"],i=["Financial Management and Accounting","External Audit","Internal Audit","Donor Audit","Compliance Audit","Certification Services","File Study, Planning at Office and Report Writing"];export{i as N,n as S,c as U};
--- assets/index-BNrbCjC-.js
+++
@@ -1,1 +0,0 @@
-import{p as r,q as t}from"./index-XVBkLyaR.js";var a=t();const e=r(a);export{e as R,a as r};
