e2a3bad1570f5e841d664f448fcb72e57191863c
Anil Sapkota <Anil99senchury@gmail.com>
2026-04-19 21:50:04 +0545

    Updates

--- assets/index-75QzvFwG.js
+++
@@ -1,1 +0,0 @@
-import{p as r,q as t}from"./index-BoXW1u2f.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ assets/taskTypeChecklists-DONq64Ap.js
@@ -0,0 +1,31 @@
+import{c as e,v as t,aG as c,aH as a}from"./index-7ECWNabv.js";/**
+ * @license lucide-react v0.563.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m15.477 12.89 1.515 8.526a.5.5 0 0 1-.81.47l-3.58-2.687a1 1 0 0 0-1.197 0l-3.586 2.686a.5.5 0 0 1-.81-.469l1.514-8.526",key:"1yiouv"}],["circle",{cx:"12",cy:"8",r:"6",key:"1vp47v"}]],h=e("award",o);/**
+ * @license lucide-react v0.563.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M5 21v-6",key:"1hz6c0"}],["path",{d:"M12 21V3",key:"1lcnhd"}],["path",{d:"M19 21V9",key:"unv183"}]],p=e("chart-no-axes-column",s);/**
+ * @license lucide-react v0.563.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["path",{d:"M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z",key:"1oefj6"}],["path",{d:"M14 2v5a1 1 0 0 0 1 1h5",key:"wfsgrz"}],["circle",{cx:"11.5",cy:"14.5",r:"2.5",key:"1bq0ko"}],["path",{d:"M13.3 16.3 15 18",key:"2quom7"}]],T=e("file-search",l);/**
+ * @license lucide-react v0.563.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M12 3v18",key:"108xh3"}],["path",{d:"m19 8 3 8a5 5 0 0 1-6 0zV7",key:"zcdpyk"}],["path",{d:"M3 7h1a17 17 0 0 0 8-2 17 17 0 0 0 8 2h1",key:"1yorad"}],["path",{d:"m5 8 3 8a5 5 0 0 1-6 0zV7",key:"eua70x"}],["path",{d:"M7 21h10",key:"1b0cd5"}]],i=e("scale",n);/**
+ * @license lucide-react v0.563.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],k=e("send",d);/**
+ * @license lucide-react v0.563.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z",key:"1s2grr"}],["path",{d:"M20 2v4",key:"1rf3ol"}],["path",{d:"M22 4h-4",key:"gwowj6"}],["circle",{cx:"4",cy:"20",r:"2",key:"6kqj1y"}]],N=e("sparkles",r),A={all:["templates"]},_=()=>t({queryKey:A.all,queryFn:()=>c.getAllTemplates(),staleTime:1e3*60}),I={[a.INTERNAL_AUDIT]:"Internal Audit",[a.STATUTORY_AUDIT]:"Statutory Audit",[a.COMPLIANCE_AUDIT]:"Compliance Audit",[a.CERTIFICATION_SERVICE]:"Certification Service",[a.FINANCIAL_MANAGEMENT]:"Financial Management and Accounting",[a.GENERAL]:"General",[a.OTHER]:"Other"},E={[a.INTERNAL_AUDIT]:"ShieldCheck",[a.STATUTORY_AUDIT]:"Scale",[a.COMPLIANCE_AUDIT]:"ClipboardCheck",[a.CERTIFICATION_SERVICE]:"Award",[a.FINANCIAL_MANAGEMENT]:"BarChart2",[a.GENERAL]:"Activity",[a.OTHER]:"FolderOpen"};export{h as A,p as C,T as F,k as S,I as T,N as a,i as b,E as c,A as t,_ as u};
