ac92852b5412d32807e7e2145d515b8c3d29f64c
Anil Sapkota <Anil99senchury@gmail.com>
2026-04-26 16:09:48 +0545

    Updates

--- assets/useClients-D-tMetkH.js
+++
@@ -1,1 +0,0 @@
-import{p as e,A as s}from"./index-BwC3JZfX.js";const t={all:["clients"]},r=()=>e({queryKey:t.all,queryFn:s.getAllClients,staleTime:1e3*60*10});export{t as c,r as u};
--- assets/useStaff-DE2xa5VB.js
+++
@@ -1,1 +0,0 @@
-import{p as e,A as s}from"./index-BwC3JZfX.js";const r={all:["users"]},a=()=>e({queryKey:r.all,queryFn:s.getAllUsers,staleTime:1e3*60*15});export{a,r as u};
